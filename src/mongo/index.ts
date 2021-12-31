a55392589fea55f01a728d65f677cd013320b91c
Craig <craigmiller160@gmail.com>
2021-12-31 17:28:05 -0500

    Fixed lint errors

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -36,23 +36,21 @@ const envToMongoEnv = ([
 const connectToMongoose = (
 	connectionString: string
 ): TE.TaskEither<Error, typeof mongoose> =>
-	TE.tryCatch(
-		() => mongoose.connect(connectionString),
-		unknownToError
-	);
+	TE.tryCatch(() => mongoose.connect(connectionString), unknownToError);
 
-export const connectToMongo = () => pipe(
-	O.sequenceArray([
-		O.fromNullable(process.env.MONGO_HOSTNAME),
-		O.fromNullable(process.env.MONGO_PORT),
-		O.fromNullable(process.env.MONGO_USER),
-		O.fromNullable(process.env.MONGO_PASSWORD),
-		O.fromNullable(process.env.MONGO_ADMIN_DB),
-		O.fromNullable(process.env.MONGO_DB)
-	]),
-	O.map(envToMongoEnv),
-	O.map(createConnectionString),
-	TE.fromOption(() => new Error('')),
-	TE.chain(connectToMongoose),
-	TE.fromIO(logInfo('Connected to MongoDB'))
-);
+export const connectToMongo = () =>
+	pipe(
+		O.sequenceArray([
+			O.fromNullable(process.env.MONGO_HOSTNAME),
+			O.fromNullable(process.env.MONGO_PORT),
+			O.fromNullable(process.env.MONGO_USER),
+			O.fromNullable(process.env.MONGO_PASSWORD),
+			O.fromNullable(process.env.MONGO_ADMIN_DB),
+			O.fromNullable(process.env.MONGO_DB)
+		]),
+		O.map(envToMongoEnv),
+		O.map(createConnectionString),
+		TE.fromOption(() => new Error('')),
+		TE.chain(connectToMongoose),
+		TE.fromIO(logInfo('Connected to MongoDB'))
+	);
