e41da870e05b588c660f5460ffd018d73df39038
Craig <craigmiller160@gmail.com>
2021-12-31 16:55:16 -0500

    Got port from env variable

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 8080;
+const port = parseInt(process.env.EXPRESS_PORT ?? '8080');
 
 app.listen(port, () => {
     console.log(`Market Tracker API listening on port ${port}`); // TODO replace with real logging
