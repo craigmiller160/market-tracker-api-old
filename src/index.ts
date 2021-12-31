35ffcccf5ac6c5babc792ac3d2025e16442dc5b3
Craig <craigmiller160@gmail.com>
2021-12-31 17:00:24 -0500

    Making progress setting up logger

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const app = express();
 const port = parseInt(process.env.EXPRESS_PORT ?? '8080');
 
 app.listen(port, () => {
-    console.log(`Market Tracker API listening on port ${port}`); // TODO replace with real logging
+	console.log(`Market Tracker API listening on port ${port}`); // TODO replace with real logging
 });
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,25 +1,31 @@
-import { createLogger, transports, format } from 'winston';
+import { createLogger, transports, format, Logger } from 'winston';
+import * as IO from 'fp-ts/IO';
 
 const myFormat = format.printf(
-    ({ level, message, timestamp, stack }) =>
-        `[${timestamp}] [${level}] - ${stack ?? message}`
+	({ level, message, timestamp, stack }) =>
+		`[${timestamp}] [${level}] - ${stack ?? message}`
 );
 
-export const logger = createLogger({
-    level: 'debug',
-    format: format.combine(
-        format((info) => {
-            info.level = info.level.toUpperCase();
-            return info;
-        })(),
-        format.errors({
-            stack: true
-        }),
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss.SSS'
-        }),
-        format.colorize(),
-        myFormat
-    ),
-    transports: [new transports.Console()]
+const logger = createLogger({
+	level: 'debug',
+	format: format.combine(
+		format((info) => {
+			info.level = info.level.toUpperCase();
+			return info;
+		})(),
+		format.errors({
+			stack: true
+		}),
+		format.timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss.SSS'
+		}),
+		format.colorize(),
+		myFormat
+	),
+	transports: [new transports.Console()]
 });
+
+export const info =
+	(message: string): IO.IO<Logger> =>
+	() =>
+		logger.info(message);
