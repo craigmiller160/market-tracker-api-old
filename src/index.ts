ae1811e01fbdc9275c1954d904ef6e7ad0d79373
Craig <craigmiller160@gmail.com>
2021-12-31 17:05:56 -0500

    More code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-import { flow } from 'fp-ts/function';
 import { logInfo } from './logger';
 
 const app = express();
 
 const port = parseInt(process.env.EXPRESS_PORT ?? '8080');
-
-
 
 app.listen(port, logInfo(`Market Tracker API listening on port ${port}`));
