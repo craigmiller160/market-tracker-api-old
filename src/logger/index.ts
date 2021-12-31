60dd147832553b0429d7577e6e547d67bc118bfc
Craig <craigmiller160@gmail.com>
2021-12-31 16:57:06 -0500

    Setup real logger

---
+++ src/logger/index.ts
@@ -0,0 +1,25 @@
+import { createLogger, transports, format } from 'winston';
+
+const myFormat = format.printf(
+    ({ level, message, timestamp, stack }) =>
+        `[${timestamp}] [${level}] - ${stack ?? message}`
+);
+
+export const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+        format((info) => {
+            info.level = info.level.toUpperCase();
+            return info;
+        })(),
+        format.errors({
+            stack: true
+        }),
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss.SSS'
+        }),
+        format.colorize(),
+        myFormat
+    ),
+    transports: [new transports.Console()]
+});
