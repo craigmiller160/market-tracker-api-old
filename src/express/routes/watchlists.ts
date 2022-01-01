d7e576f0c78a721392271ebad755555559c86d01
Craig <craigmiller160@gmail.com>
2022-01-01 10:57:05 -0500

    More progress

--- src/express/routes/watchlists.ts
+++ src/express/routes/watchlists.ts
@@ -1,9 +1,20 @@
 import { RouteCreator } from './RouteCreator';
+import { pipe } from 'fp-ts/function';
+import { findWatchlistsForUser, saveWatchlistsForUser } from '../../services/mongo/WatchlistService';
+import * as TE from 'fp-ts/TaskEither';
+import { Request } from 'express';
+import { Watchlist } from '../../mongo/models/WatchlistModel';
 
-export const getWatchlists: RouteCreator = (app) => {
-	throw new Error();
-};
+export const getWatchlists: RouteCreator = (app) =>
+	app.get('/watchlists', (req, res) =>
+		pipe(
+			findWatchlistsForUser(),
+			TE.map((_) => res.json(_))
+		)()
+	);
 
-export const saveWatchlists: RouteCreator = (app) => {
-	throw new Error();
-};
+export const saveWatchlists: RouteCreator = (app) =>
+	app.post('/watchlists', (req: Request<unknown,unknown,Watchlist[]>, res) => pipe(
+		saveWatchlistsForUser(req.body),
+		TE.map((_) => res.json(_))
+	)())
