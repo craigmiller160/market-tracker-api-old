d9c2bca34b620b55fdd7d1508b367d80bdc02956
Craig <craigmiller160@gmail.com>
2022-01-01 15:03:04 -0500

    Stubbed out tests

---
+++ test/express/routes/portfolios.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('portfolios', () => {
+	it('getPortfolios', () => {
+		throw new Error();
+	});
+
+	it('savePortfolios', () => {
+		throw new Error();
+	});
+});
---
+++ test/express/routes/watchlists.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('watchlists route', () => {
+	it('getWatchlists', () => {
+		throw new Error();
+	});
+
+	it('saveWatchlists', () => {
+		throw new Error();
+	});
+});
