38114eddbc7236253dd9d08a29014e90fe478f91
Craig <craigmiller160@gmail.com>
2022-01-01 16:14:49 -0500

    More improvements to test logic

--- src/function/TaskEitherUtils.ts
+++ src/function/TaskEitherUtils.ts
@@ -1,7 +1,22 @@
 import * as TE from 'fp-ts/TaskEither';
+import * as T from 'fp-ts/Task';
 import { unknownToError } from './unknownToError';
+import { pipe } from 'fp-ts/function';
 
 export const tryCatch = <T>(fn: () => Promise<T>): TE.TaskEither<Error, T> =>
 	TE.tryCatch(fn, unknownToError);
 
 export type TaskEither<T> = TE.TaskEither<Error, T>;
+
+export const mapOrThrow =
+	<T, R>(fn: (t: T) => R) =>
+	(te: TaskEither<T>): T.Task<R> =>
+		pipe(
+			te,
+			TE.fold(
+				(_) => {
+					throw _;
+				},
+				(_) => T.of(fn(_))
+			)
+		);
--- test/express/routes/portfolios.test.ts
+++ test/express/routes/portfolios.test.ts
@@ -4,23 +4,17 @@ import {
 	stopMongoTestServer
 } from '../../testutils/mongoServer';
 import { pipe } from 'fp-ts/function';
-import * as TE from 'fp-ts/TaskEither';
-import * as T from 'fp-ts/Task';
+import * as TEU from '../../../src/function/TaskEitherUtils';
 
 describe('portfolios', () => {
 	let mongoTestServer: MongoTestServer;
 	beforeAll(async () => {
 		await pipe(
 			createMongoTestServer(),
-			TE.fold(
-				(_) => {
-					throw _;
-				},
-				(_) => {
-					mongoTestServer = _;
-					return T.of(_);
-				}
-			)
+			TEU.mapOrThrow((_) => {
+				mongoTestServer = _;
+				return _;
+			})
 		)();
 	});
 
