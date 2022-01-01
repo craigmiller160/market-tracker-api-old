f4b861c1abead7ba475d56b673e9d881686de4c8
Craig <craigmiller160@gmail.com>
2021-12-31 19:05:50 -0500

    Setup portfolio get request

--- src/express/routes/portfolios.ts
+++ src/express/routes/portfolios.ts
@@ -1,7 +1,9 @@
 import { RouteCreator } from './RouteCreator';
+import { PortfolioModel } from '../../mongo/models/PortfolioModel';
 
 export const getPortfolios: RouteCreator = (app) => {
-	app.get('/portfolios', (req, res) => {
-		throw new Error();
+	app.get('/portfolios', async (req, res) => {
+        const result = await PortfolioModel.find().exec();
+        res.json(result);
 	});
 };
