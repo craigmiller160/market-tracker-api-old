078d38701f0ea9d81487c798f98f92a81eafe0f2
Craig <craigmiller160@gmail.com>
2022-01-01 09:53:47 -0500

    More progress

--- src/services/mongo/PortfolioService.ts
+++ src/services/mongo/PortfolioService.ts
@@ -1,12 +1,16 @@
 import * as TE from 'fp-ts/TaskEither';
 import { Portfolio } from '../../mongo/models/Portfolio';
-import { PortfolioModel } from '../../mongo/models/PortfolioModel';
+import {
+	PortfolioModel,
+	PortfolioModelType
+} from '../../mongo/models/PortfolioModel';
 import { unknownToError } from '../../function/unknownToError';
 import { pipe } from 'fp-ts/function';
 import * as A from 'fp-ts/Array';
 import * as E from 'fp-ts/Either';
 import * as EU from '../../function/EitherUtils';
 import * as TEU from '../../function/TaskEitherUtils';
+import * as MO from '../../function/Mongoose';
 
 const getCurrentUserId = () => 1;
 
@@ -19,6 +23,17 @@ export const findPortfoliosForUser = (): TEU.TaskEither<
 		unknownToError
 	);
 };
+
+const replacePortfoliosForUser = (
+	userId: number,
+	portfolioModels: PortfolioModelType[]
+): TEU.TaskEither<Portfolio[]> =>
+	pipe(
+		TEU.tryCatch(PortfolioModel.deleteOne({ userId }).exec),
+		TE.chain(() =>
+			TEU.tryCatch(() => PortfolioModel.insertMany(portfolioModels))
+		)
+	);
 
 export const savePortfoliosForUser = (
 	portfolios: Portfolio[]
@@ -34,6 +49,11 @@ export const savePortfoliosForUser = (
 					userId
 				})
 		)
+	);
+
+	pipe(
+		TEU.tryCatch(PortfolioModel.startSession),
+		MO.withTransaction(replacePortfoliosForUser(userId, portfolioModels))
 	);
 
 	return pipe(
