2d110e8ece2682de998fadaaeb69092df249824e
Craig <craigmiller160@gmail.com>
2021-12-31 21:20:59 -0500

    Refactored handling of PortfolioModel lookup

--- src/express/routes/portfolios.ts
+++ src/express/routes/portfolios.ts
@@ -1,9 +1,15 @@
 import { RouteCreator } from './RouteCreator';
 import { PortfolioModel } from '../../mongo/models/PortfolioModel';
+import * as TE from 'fp-ts/TaskEither';
+import { Portfolio } from '../../mongo/models/Portfolio';
+import { unknownToError } from '../../function/unknownToError';
+import { pipe } from 'fp-ts/function';
+
+const findPortfolioModel = (): TE.TaskEither<Error, Portfolio[]> =>
+	TE.tryCatch(() => PortfolioModel.find().exec(), unknownToError);
 
 export const getPortfolios: RouteCreator = (app) => {
 	app.get('/portfolios', async (req, res) => {
-        const result = await PortfolioModel.find().exec();
-        res.json(result);
+		pipe(findPortfolioModel(), TE.map(res.json));
 	});
 };
