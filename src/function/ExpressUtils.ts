08146add0152469e09009036d78404791790df1c
Craig <craigmiller160@gmail.com>
2022-01-01 10:21:43 -0500

    Working on new utility function

--- src/function/ExpressUtils.ts
+++ src/function/ExpressUtils.ts
@@ -1,4 +1,19 @@
+import * as core from 'express-serve-static-core';
+import { Request, Response } from 'express';
 
-export const route = (req: any, res: any) => {
+export interface RouteContext {
+	readonly req: any;
+	readonly res: any;
+}
 
-};
+export type ParamsType = core.ParamsDictionary;
+export type QueryType = core.Query;
+
+export const route =
+	<Params = ParamsType, ReqBody = any, ReqQuery = QueryType>(
+		fn: () => void
+	) =>
+	(
+		req: Request<Params, any, ReqBody, ReqQuery, Record<string, any>>,
+		res: Response<any, Record<string, any>>
+	) => {};
