11ae0bbf80597db10b0aaeb7b590635cc3e5128d
Craig <craigmiller160@gmail.com>
2022-01-01 10:42:56 -0500

    Stubbed out more routes

---
+++ src/express/routes/watchlists.ts
@@ -0,0 +1,9 @@
+import { RouteCreator } from './RouteCreator';
+
+export const getWatchlists: RouteCreator = (app) => {
+    throw new Error();
+}
+
+export const saveWatchlists: RouteCreator = (app) => {
+    throw new Error();
+};
