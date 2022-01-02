f78a397407f530a23a0bccb835f8ebd577bd8931
Craig Miller <craigmiller160@gmail.com>
2022-01-02 14:17:56 -0500

    Husky test 2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { connectToMongo } from './mongo';
 import { pipe } from 'fp-ts/function';
-import * as TE from 'fp-ts/TaskEither';
-import { startExpressServer } from './express';
+import * as TE from 'fp-ts/TaskEither'
+import { startExpressServer } from './express'
 
 pipe(
 	connectToMongo(),
