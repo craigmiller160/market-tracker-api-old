9ddafdd23fc34724ec381ea092c57e4be75a7fa2
Craig Miller <craigmiller160@gmail.com>
2022-01-02 18:24:23 -0500

    Integrated healthcheck endpoint

--- src/express/routes/index.ts
+++ src/express/routes/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
 import { getPortfolios, savePortfolios } from './portfolios';
 import { getWatchlists, saveWatchlists } from './watchlists';
+import { healthcheck } from './healthcheck';
 
 export const createRoutes = (app: Express) => {
 	getPortfolios(app);
 	savePortfolios(app);
 	getWatchlists(app);
 	saveWatchlists(app);
+	healthcheck(app);
 };
