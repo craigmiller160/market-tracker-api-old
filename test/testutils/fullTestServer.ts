34b80a93e18a2644468483bd38f6e542d9a23e76
Craig Miller <craigmiller160@gmail.com>
2022-01-02 16:10:54 -0500

    Setting up full test server

---
+++ test/testutils/fullTestServer.ts
@@ -0,0 +1,18 @@
+import {ExpressServer, startExpressServer} from '../../src/express';
+import {createMongoTestServer, MongoTestServer} from './mongoServer';
+import {pipe} from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import * as TEU from '../../src/function/TaskEitherUtils';
+
+export interface FullTestServer {
+    readonly expressServer: ExpressServer;
+    readonly mongoServer: MongoTestServer;
+}
+
+export const createFullTestServer = (): Promise<FullTestServer> =>
+    pipe(
+        createMongoTestServer(),
+        TE.bindTo('mongoServer'),
+        TE.bind('expressServer', startExpressServer),
+        TEU.throwIfLeft
+    )();
