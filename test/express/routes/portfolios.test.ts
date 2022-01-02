60d9fa3cebdd37e8a753784b6ce3a9d3fd67bcd7
Craig <craigmiller160@gmail.com>
2022-01-02 09:09:01 -0500

    Much more progress with first test  # On branch feature/MT-3

--- test/express/routes/portfolios.test.ts
+++ test/express/routes/portfolios.test.ts
@@ -9,10 +9,12 @@ import * as T from 'fp-ts/Task';
 import * as EU from '../../../src/function/EitherUtils';
 import { ExpressServer, startExpressServer } from '../../../src/express';
 import request from 'supertest';
+import { Portfolio, PortfolioModel } from '../../../src/mongo/models/PortfolioModel';
 
 describe('portfolios', () => {
 	let mongoTestServer: MongoTestServer;
 	let expressServer: ExpressServer;
+	let user1InitPortfolios: Portfolio[];
 	beforeAll(async () => {
 		await pipe(
 			createMongoTestServer(),
@@ -30,8 +32,37 @@ describe('portfolios', () => {
 		expressServer[0].close();
 	});
 
+	beforeEach(() => {
+		user1InitPortfolios = [
+			{
+				userId: 1,
+				portfolioName: 'One',
+				stocks: ['ABC', 'DEF'],
+				cryptos: ['GHI']
+			},
+			{
+				userId: 1,
+				portfolioName: 'Two',
+				stocks: ['QRS', 'TUV'],
+				cryptos: ['WXYZ']
+			}
+		];
+		const user1Models = user1InitPortfolios.map((_) => new PortfolioModel(_));
+		PortfolioModel.insertMany(user1Models);
+	});
+
+	afterEach(() => {
+		PortfolioModel.deleteMany().exec();
+	})
+
 	it('getPortfolios', async () => {
-		await request(expressServer[0]).get('/portfolios').expect(200);
+		const res = await request(expressServer[0])
+			.get('/portfolios')
+			.expect(200);
+		expect(res.body).toEqual([
+			expect.objectContaining(user1InitPortfolios[0]),
+			expect.objectContaining(user1InitPortfolios[1])
+		]);
 	});
 
 	it('savePortfolios', () => {
