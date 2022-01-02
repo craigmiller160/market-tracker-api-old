18ad26d49cf3a13091bb74e7e429339097787502
Craig <craigmiller160@gmail.com>
2022-01-02 08:42:42 -0500

    Making progress on tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ const jestConfig = require('@craigmiller160/jest-config');
 const jestTsConfig = require('@craigmiller160/jest-config-ts');
 const merge = require('@craigmiller160/config-merge');
 
-module.exports = merge(jestConfig, jestTsConfig);
+module.exports = merge(jestConfig, jestTsConfig, {
+    testEnvironment: 'node'
+});
