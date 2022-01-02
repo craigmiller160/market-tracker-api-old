ddf0974811bc493cd2e5a7428470169ff9cc08c7
Craig <craigmiller160@gmail.com>
2022-01-02 08:56:01 -0500

    Got app to connect to primary MongoDB instance

--- src/mongo/connectionString.ts
+++ src/mongo/connectionString.ts
@@ -2,6 +2,8 @@ import * as EU from '../function/EitherUtils';
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import * as E from 'fp-ts/Either';
+import { match } from 'ts-pattern';
+import { logDebug } from '../logger';
 
 interface MongoEnv {
 	readonly hostname: string;
@@ -13,7 +15,15 @@ interface MongoEnv {
 }
 
 const createConnectionString = (env: MongoEnv): string =>
-	`mongodb://${env.user}:${env.password}@${env.hostname}:${env.port}/${env.db}?authSource=${env.adminDb}`;
+	`mongodb://${env.user}:${env.password}@${env.hostname}:${env.port}/${env.db}?authSource=${env.adminDb}&tls=true&tlsAllowInvalidCertificates=true&tlsAllowInvalidHostnames=true`;
+
+const logConnectionStringInDev = (connectionString: string): string =>
+	match(process.env.NODE_ENV)
+		.with('development', () => {
+			logDebug(`Mongo Connection String: ${connectionString}`)();
+			return connectionString;
+		})
+		.otherwise(() => connectionString);
 
 const envToMongoEnv = ([
 	hostname,
@@ -31,18 +41,26 @@ const envToMongoEnv = ([
 	db
 });
 
+const getMongoPasswordEnv = (): O.Option<string> =>
+	pipe(
+		O.fromNullable(process.env.MONGO_PASSWORD),
+		O.getOrElse(() => process.env.MONGO_ROOT_PASSWORD),
+		O.fromNullable
+	);
+
 export const getConnectionString = (): EU.Either<string> => {
 	return pipe(
 		O.sequenceArray([
 			O.fromNullable(process.env.MONGO_HOSTNAME),
 			O.fromNullable(process.env.MONGO_PORT),
 			O.fromNullable(process.env.MONGO_USER),
-			O.fromNullable(process.env.MONGO_PASSWORD),
+			getMongoPasswordEnv(),
 			O.fromNullable(process.env.MONGO_AUTH_DB),
 			O.fromNullable(process.env.MONGO_DB)
 		]),
 		O.map(envToMongoEnv),
 		O.map(createConnectionString),
+		O.map(logConnectionStringInDev),
 		E.fromOption(
 			() =>
 				new Error('Missing environment variables for Mongo connection')
