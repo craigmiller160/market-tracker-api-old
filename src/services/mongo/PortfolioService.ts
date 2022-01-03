ad106ef62eb93eb1643954c8b43e9279360d787a
Craig Miller <craigmiller160@gmail.com>
2022-01-03 15:54:04 -0500

    Making some progress refactoring how the IO is handled

--- src/services/mongo/PortfolioService.ts
+++ src/services/mongo/PortfolioService.ts
@@ -16,21 +16,14 @@ export const findPortfoliosForUser = (): TEU.TaskEither<Portfolio[]> => {
 	return TEU.tryCatch(() => PortfolioModel.find({ userId }).exec());
 };
 
-const replacePortfoliosForUser = (
-	userId: number,
-	portfolioModels: PortfolioModelInstanceType[]
-): TEU.TaskEither<Portfolio[]> =>
-	pipe(
-		TEU.multiTypeSequence(
-			TEU.tryCatch(() => PortfolioModel.deleteMany({ userId }).exec()),
-			TEU.tryCatch(async () => PortfolioModel.insertMany(portfolioModels))
-		),
-		TE.map(([, _]) => _)
-	);
+const replacePortfoliosForUser = async (userId: number, portfolioModels: PortfolioModelInstanceType[]): Promise<void> => {
+	await PortfolioModel.deleteMany({ userId }).exec();
+	await PortfolioModel.insertMany(portfolioModels);
+}
 
 export const savePortfoliosForUser = (
 	portfolios: Portfolio[]
-): TEU.TaskEither<void> => {
+): TEU.TaskEither<unknown> => {
 	const userId = getCurrentUserId();
 
 	const portfolioModels = pipe(
@@ -43,15 +36,23 @@ export const savePortfoliosForUser = (
 		)
 	);
 
-	return pipe(
-		TEU.tryCatch(() => PortfolioModel.startSession()),
+	const sessionTE = TEU.tryCatch(() => PortfolioModel.startSession());
+
+	const postTxnTE = pipe(
+		sessionTE,
 		TE.chainFirst((session) =>
 			TEU.tryCatch(() =>
 				session.withTransaction(
-					replacePortfoliosForUser(userId, portfolioModels)
+					() => replacePortfoliosForUser(userId, portfolioModels)
 				)
 			)
-		),
+		)
+	);
+
+	pipe(
+		sessionTE,
 		TE.chain((session) => TEU.tryCatch(() => session.endSession()))
 	);
+
+	return postTxnTE;
 };
