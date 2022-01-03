4d6be24efe49176c2502787e2d3e05bd466ef8d1
Craig Miller <craigmiller160@gmail.com>
2022-01-03 16:13:15 -0500

    Implemented timestamp into error response

--- src/express/errorHandler.ts
+++ src/express/errorHandler.ts
@@ -1,14 +1,16 @@
 import { Express, NextFunction, Request, Response } from 'express';
 import { logger } from '../logger';
 import qs from 'qs';
+import { format } from 'date-fns';
 
 interface ErrorResponse {
+	readonly timestamp: string;
 	readonly status: number;
 	readonly message: string;
 	readonly request: string;
 }
 
-const getErrorStatus = (err: Error): number => 500;
+const getErrorStatus = (): number => 500;
 
 const createErrorResponse = (
 	err: Error,
@@ -18,7 +20,10 @@ const createErrorResponse = (
 	const queryString = qs.stringify(req.query);
 	const fullQueryString = queryString.length > 0 ? `?${queryString}` : '';
 
+	const timestamp = format(new Date(), 'yyyy-MM-dd HH:mm:ss.SSS');
+
 	return {
+		timestamp,
 		status,
 		message: err.message,
 		request: `${req.method} ${req.path}${fullQueryString}`
@@ -30,7 +35,7 @@ export const setupErrorHandler = (app: Express) =>
 		logger.error('Error while processing request');
 		logger.error(err);
 
-		const status = getErrorStatus(err);
+		const status = getErrorStatus();
 		const errorResponse = createErrorResponse(err, req, status);
 		res.status(status);
 		res.json(errorResponse);
