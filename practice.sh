25522cd91485588cee50693b11399534265b3e45
Craig Miller <craigmiller160@gmail.com>
2022-01-04 16:12:45 -0500

    Seeing if this works

--- practice.sh
+++ practice.sh
@@ -6,4 +6,21 @@ NC='\033[0m'
 
 old_tracked=$(git status --porcelain | grep -E '^(A|M).*$' | sed -E 's/(A|M) *//g')
 
-echo $old_tracked
+yarn lint
+
+new_untracked=$(git status --porcelain | grep -E '^\SM.*$' | sed 's/\SM //g')
+
+for newFile in $new_untracked; do
+  match=false
+  for oldFile in $old_tracked; do
+    if [ $oldFile = $newFile ]; then
+      match=true
+      break
+    fi
+  done
+
+  if [ $match = true ] ; then
+    printf "${GREEN}Adding lint formatting changes: $newFile\n${NC}"
+    git add $newFile
+  fi
+done
