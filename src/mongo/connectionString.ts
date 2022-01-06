d30eb45a11d3decb18ffbefcaf62836af4812780
Craig Miller <craigmiller160@gmail.com>
2022-01-06 17:53:56 -0500

    Added connection string logging

--- src/mongo/connectionString.ts
+++ src/mongo/connectionString.ts
@@ -20,7 +20,8 @@ const createConnectionString = (env: MongoEnv): string =>
 
 const logConnectionStringInDev = (connectionString: string): string =>
 	match(process.env.NODE_ENV)
-		.with('development', () => {
+		.with('production', () => {
+			// TODO change back to development
 			logDebug(`Mongo Connection String: ${connectionString}`)();
 			return connectionString;
 		})
