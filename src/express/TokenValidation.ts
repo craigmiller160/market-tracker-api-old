85fadea983d5012054e47bbe06b0f7f6d41c2d05
Craig <craigmiller160@gmail.com>
2022-01-08 15:08:45 -0500

    Setup TokenValidation for first passport attempt

--- src/express/TokenValidation.ts
+++ src/express/TokenValidation.ts
@@ -1,5 +1,23 @@
+import {
+	Strategy as JwtStrategy,
+	ExtractJwt,
+	StrategyOptions
+} from 'passport-jwt';
+import { TokenKey } from '../auth/TokenKey';
 import passport from 'passport';
 
-export const createPassportValidation = () => {
-    // TODO finish this
+// TODO do I need tests for this?
+
+export const createPassportValidation = (tokenKey: TokenKey) => {
+	const options: StrategyOptions = {
+		secretOrKey: tokenKey.key,
+		// TODO need to add cookie support as well
+		jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+	};
+
+	passport.use(
+		new JwtStrategy(options, (payload, done) => {
+			done(null, 'Success');
+		})
+	);
 };
