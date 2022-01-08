eb94d3a378efb7db61dcebf4641ae46155e3dba6
Craig <craigmiller160@gmail.com>
2022-01-08 14:32:27 -0500

    Beginning progress with insecure paths

---
+++ src/auth/InsecurePaths.ts
@@ -0,0 +1,21 @@
+import * as O from 'fp-ts/Option';
+import * as A from 'fp-ts/Array';
+import { pipe } from 'fp-ts/function';
+
+export interface InsecurePaths {
+	readonly paths: string[];
+}
+
+const DEFAULT_INSECURE_PATHS = ['/login', '/logout'];
+
+const getInsecurePathsEnv = (): string[] =>
+	pipe(
+		O.fromNullable(process.env.INSECURE_PATHS),
+		O.map((_) => _.split(',')),
+		O.getOrElse((): string[] => []),
+		A.map((_) => _.trim())
+	);
+
+export const getInsecurePaths = () => {
+	getInsecurePathsEnv();
+};
