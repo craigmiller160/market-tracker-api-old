08aa2135a5d9c61e1a442ce3cc5cbeca7a356d5e
Craig <craigmiller160@gmail.com>
2022-01-08 13:55:59 -0500

    Working on test setup

--- test/auth/loadJwk.test.ts
+++ test/auth/loadJwk.test.ts
@@ -1,6 +1,14 @@
 export {};
 
 describe('loadJwk', () => {
+	beforeEach(() => {
+		process.env.AUTH_SERVER_HOST = 'https://authServerHost';
+	});
+
+	afterEach(() => {
+		process.env.AUTH_SERVER_HOST = undefined;
+	});
+
 	it('loads the JWK as a PEM', () => {
 		throw new Error();
 	});
