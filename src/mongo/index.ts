7f721587ca8f8ff6d8108435142a6b7d432e76d4
Craig Miller <craigmiller160@gmail.com>
2022-01-08 10:22:46 -0500

    Cleaned up connectToMongo

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -2,39 +2,18 @@ import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import * as TEU from '../function/TaskEitherUtils';
 import mongoose, { Mongoose } from 'mongoose';
-import { logger, logInfo } from '../logger';
+import { logInfo } from '../logger';
 import { getConnectionString } from './connectionString';
-import { MongoClient } from 'mongodb';
 
 const connectToMongoose = (
 	connectionString: string
 ): TEU.TaskEither<typeof mongoose> =>
 	TEU.tryCatch(() => mongoose.connect(connectionString));
 
-// TODO delete this whole thing
-const connectWithMongoClient = ( // eslint-disable-line
-	connectionString: string // eslint-disable-line
-): TE.TaskEither<Error, unknown> => {
-	// const tempString = 'mongodb://mongodb-service:27017';
-	logger.debug(`Trying to connect with MongoClient: ${connectionString}`);
-	return pipe(
-		TEU.tryCatch(() => MongoClient.connect(connectionString)),
-		TE.map(() => {
-			logger.debug('Successfully connected with MongoClient');
-		}),
-		TE.mapLeft((e) => {
-			logger.error('Failed to connect with MongoClient');
-			logger.error(e);
-			return e;
-		})
-	);
-};
-
 export const connectToMongo = (): TEU.TaskEither<Mongoose> =>
 	pipe(
 		getConnectionString(),
 		TE.fromEither,
-		TE.chainFirst(connectWithMongoClient),
 		TE.chain(connectToMongoose),
 		TE.chainFirst(() => TE.fromIO(logInfo('Connected to MongoDB')))
 	);
