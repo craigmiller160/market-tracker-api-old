fec3af49833aa5d9521212b74c4c2b2007f1e66c
Craig Miller <craigmiller160@gmail.com>
2022-01-09 16:34:34 -0500

    Stubbed out new route and tests

---
+++ test/express/routes/user.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('user details route', () => {
+	it('gets details for authenticated user', async () => {
+		throw new Error();
+	});
+
+	it('fails when not authenticated', async () => {
+		throw new Error();
+	});
+});
