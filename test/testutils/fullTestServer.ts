fc4ed5ee3055b3e635f5f198efa16c3486f061fb
Craig <craigmiller160@gmail.com>
2022-01-09 07:58:44 -0500

    Integrated KeyPair into FullTestServer

--- test/testutils/fullTestServer.ts
+++ test/testutils/fullTestServer.ts
@@ -1,4 +1,5 @@
 import { ExpressServer, startExpressServer } from '../../src/express';
+import { ec } from 'elliptic';
 import {
 	createMongoTestServer,
 	MongoTestServer,
@@ -8,17 +9,33 @@ import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import * as TEU from '../../src/function/TaskEitherUtils';
 import { stopExpressServer } from './expressServer';
+import { createKeyPair } from './keyPair';
+import { TokenKey } from '../../src/auth/TokenKey';
 
 export interface FullTestServer {
+	readonly keyPair: ec.KeyPair;
 	readonly expressServer: ExpressServer;
 	readonly mongoServer: MongoTestServer;
 }
 
+const createExpressServerWithKey = (
+	keyPair: ec.KeyPair
+): TE.TaskEither<Error, ExpressServer> => {
+	const tokenKey: TokenKey = {
+		key: keyPair.getPublic().encode('hex', false)
+	};
+	return startExpressServer(tokenKey);
+};
+
 export const createFullTestServer = (): Promise<FullTestServer> =>
 	pipe(
-		createMongoTestServer(),
-		TE.bindTo('mongoServer'),
-		TE.bind('expressServer', startExpressServer),
+		createKeyPair(),
+		TE.fromEither,
+		TE.bindTo('keyPair'),
+		TE.bind('mongoServer', createMongoTestServer),
+		TE.bind('expressServer', ({ keyPair }) =>
+			createExpressServerWithKey(keyPair)
+		),
 		TEU.throwIfLeft
 	)();
 
