83801acb9912f7a334b522faed2a10aa4c57899e
Craig Miller <craigmiller160@gmail.com>
2022-01-09 16:37:11 -0500

    Implemented tests

--- test/express/routes/user.test.ts
+++ test/express/routes/user.test.ts
@@ -1,11 +1,38 @@
-export {};
+import {
+	accessToken,
+	createAccessToken,
+	createFullTestServer,
+	FullTestServer,
+	stopFullTestServer
+} from '../../testutils/fullTestServer';
+import request from 'supertest';
 
 describe('user details route', () => {
+	let fullTestServer: FullTestServer;
+	beforeAll(async () => {
+		fullTestServer = await createFullTestServer();
+	});
+
+	afterAll(async () => {
+		await stopFullTestServer(fullTestServer);
+	});
+
 	it('gets details for authenticated user', async () => {
-		throw new Error();
+		const token = createAccessToken(fullTestServer.keyPair.privateKey);
+		const res = await request(fullTestServer.expressServer.server)
+			.get('/user')
+			.timeout(2000)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(200);
+		expect(res.body).toEqual({
+			...accessToken
+		});
 	});
 
 	it('fails when not authenticated', async () => {
-		throw new Error();
+		await request(fullTestServer.expressServer.server)
+			.get('/user')
+			.timeout(2000)
+			.expect(401);
 	});
 });
