7f644c61b3030b0cc04de350cd32874ae1f70023
Craig <craigmiller160@gmail.com>
2022-01-09 08:12:33 -0500

    Stubbed out new tests

---
+++ test/express/TokenValidation.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+describe('TokenValidation', () => {
+	it('has valid access token', async () => {
+		throw new Error();
+	});
+
+	it('access token is expired', async () => {
+		throw new Error();
+	});
+
+	it('access token has invalid signature', async () => {
+		throw new Error();
+	});
+
+	it('has no access token', async () => {
+		throw new Error();
+	});
+});
