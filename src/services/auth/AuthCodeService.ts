eda1fa6afe705c93178e3d804885f8e49ccea12d
Craig Miller <craigmiller160@gmail.com>
2022-01-13 17:53:52 -0500

    Working on auth code login logic

--- src/services/auth/AuthCodeService.ts
+++ src/services/auth/AuthCodeService.ts
@@ -1,6 +1,35 @@
 import { Request } from 'express';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { randomInt } from 'crypto';
 
-export const prepareAuthCodeLogin = (req: Request): string => {
-	req.header('Origin');
-	throw new Error();
+const getOrigin = (req: Request): E.Either<Error, string> =>
+	pipe(
+		O.fromNullable(req.header('Origin')),
+		E.fromOption(() => new Error('Missing origin header on request'))
+	);
+
+const storeAuthCodeLoginSessionValues = (
+	req: Request,
+	state: number,
+	origin: string
+): void => {
+	req.session.state = state;
+	req.session.origin = origin;
+	// TODO set expiration
+};
+
+const buildAuthCodeLoginUrl = (origin: string, state: number): string => {};
+
+export const prepareAuthCodeLogin = (req: Request): E.Either<Error, string> => {
+	const state = randomInt(1_000_000_000);
+	return pipe(
+		getOrigin(req),
+		E.map((origin) => {
+			storeAuthCodeLoginSessionValues(req, state, origin);
+			return origin;
+		}),
+		E.map((origin) => buildAuthCodeLoginUrl(origin, state))
+	);
 };
