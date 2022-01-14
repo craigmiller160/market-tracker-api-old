f356d947734e4e081c9660ad7341d921740b03e4
Craig Miller <craigmiller160@gmail.com>
2022-01-14 18:19:27 -0500

    Created base logic

--- src/services/mongo/RefreshTokenService.ts
+++ src/services/mongo/RefreshTokenService.ts
@@ -1,5 +1,18 @@
-import { AppRefreshToken } from '../../mongo/models/AppRefreshTokenModel';
+import {
+	AppRefreshToken,
+	AppRefreshTokenModel,
+	appRefreshTokenToModel
+} from '../../mongo/models/AppRefreshTokenModel';
 
 export const saveRefreshToken = (refreshToken: AppRefreshToken) => {
+	const refreshTokenModel = appRefreshTokenToModel(refreshToken);
+	AppRefreshTokenModel.startSession().then((session) => {
+		session.withTransaction(async () => {
+			await AppRefreshTokenModel.deleteOne({
+				tokenId: refreshToken.tokenId
+			}).exec();
+			await refreshTokenModel.save();
+		});
+	});
 	throw new Error();
 };
