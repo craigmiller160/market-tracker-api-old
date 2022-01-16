6f59bfcb2bd4a782015dbb1fabc8f7ac41f9d555
Craig Miller <craigmiller160@gmail.com>
2022-01-16 15:29:24 -0500

    Working on super barebones start

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export {}
+
+interface MongoEnv {
+    readonly hostname: string;
+    readonly port: string;
+    readonly user: string;
+    readonly password: string;
+    readonly adminDb: string;
+    readonly db: string;
+}
+
+const createConnectionString = (env: MongoEnv): string =>
+    `mongodb://${env.user}:${env.password}@${env.hostname}:${env.port}/${env.db}?authSource=${env.adminDb}&tls=true&tlsAllowInvalidCertificates=true&tlsAllowInvalidHostnames=true`;
+
+const password: string = (process.env.MONGO_PASSWORD ?? process.env.MONGO_ROOT_PASSWORD)!;
+
+const mongoEnv: MongoEnv = {
+    hostname: process.env.MONGO_HOSTNAME!,
+    port: process.env.MONGO_PORT!,
+    user: process.env.MONGO_USER!,
+    password,
+    adminDb: process.env.MONGO_AUTH_DB!,
+    db: process.env.MONGO_DB!
+}
+
+const connectionString = createConnectionString(mongoEnv);
+console.log('Connection String', connectionString);
