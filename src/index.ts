82f6e355bd27c54d46cba15d2deafe5defb9b7cc
Craig Miller <craigmiller160@gmail.com>
2022-01-16 15:26:31 -0500

    Progress

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,20 @@
-// import './processErrorHandling';
-// import { connectToMongo } from './mongo';
-// import { pipe } from 'fp-ts/function';
-// import * as TE from 'fp-ts/TaskEither';
-// import { startExpressServer } from './express';
-// import { logError, logInfo } from './logger';
-// import { loadTokenKey } from './auth/TokenKey';
-//
-// logInfo('Starting application')();
+import './processErrorHandling';
+import { connectToMongo } from './mongo';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import { startExpressServer } from './express';
+import { logError, logInfo } from './logger';
+import { loadTokenKey } from './auth/TokenKey';
 
-// pipe(
-// 	loadTokenKey(),
-// 	TE.chainFirst(connectToMongo),
-// 	TE.chain(startExpressServer),
-// 	TE.mapLeft((_) => {
-// 		logError('Error starting application', _)();
-// 		process.exit(1);
-// 		return _;
-// 	})
-// )();
+logInfo('Starting application')();
 
-/* eslint-disable */
-
-import {getConnectionString} from './mongo/connectionString';
-import {pipe} from 'fp-ts/function';
-import * as E from 'fp-ts/Either';
-import { identity } from 'fp-ts/function';
-import mongoose from 'mongoose';
-
-const connectString = pipe(
-	getConnectionString(),
-	E.fold(
-		(ex) => {
-			throw ex
-		},
-		identity
-	)
-);
-
-console.log('Connecting to Mongoose', connectString);
-mongoose.connect(connectString)
-	.then(() => {
-		console.log('Successfully connected');
+pipe(
+	loadTokenKey(),
+	TE.chainFirst(connectToMongo),
+	TE.chain(startExpressServer),
+	TE.mapLeft((_) => {
+		logError('Error starting application', _)();
+		process.exit(1);
+		return _;
 	})
-	.catch((ex) => {
-		console.error('Error connecting');
-		console.error(ex);
-	})
+)();
